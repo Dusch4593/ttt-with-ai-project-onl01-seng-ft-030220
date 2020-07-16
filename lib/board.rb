582dd843af58ba23f94b220461cc76e687294109
Dusch4593 <brandondusch@gmail.com>
2020-07-16 00:59:29 -0400

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,47 @@
+class Board
+  attr_accessor :cells
+
+  def initialize
+    self.reset!
+  end
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def reset!
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def position(input)
+    self.cells[input.to_i-1]
+  end
+
+  def update(pos, player)
+    if !taken?(self.cells[pos.to_i-1])
+      self.cells[pos.to_i-1] = player.token
+    end
+  end
+
+  def full?
+    self.cells.all? {|cell| cell == "X" || cell == "O"}
+  end
+
+  def turn_count
+    self.cells.count {|cell|
+      cell == "X" || cell == "O"
+    }
+  end
+
+  def taken?(position)
+    self.cells[position.to_i-1] == "X" || self.cells[position.to_i-1] == "O"
+  end
+
+  def valid_move?(position)
+    !taken?(position) && position.to_i.between?(1,9)
+  end
+end
---
+++ lib/players/human.rb
@@ -0,0 +1,8 @@
+module Players
+  class Human < Player
+    def move(board)
+      print "Select a position on the board (#{self.token}): "
+      input = gets.strip
+    end
+  end
+end
